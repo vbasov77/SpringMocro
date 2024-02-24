3e7ffb97f720fdfe13751b323ff9b6303b895c58
Vbasov <0120912@mail.ru>
2024-02-24 11:23:34 +0300

    add gateway

---
+++ gateway_service/src/test/java/ru/gb/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
